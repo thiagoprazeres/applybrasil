c3f2163c0cccee1b5ef7db635f7b02b4f96b12ce
Thiago Prazeres <thiagoprazeres@gmail.com>
2026-01-24 17:29:17 -0300

    add imagem og

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), netlify()],
 });
